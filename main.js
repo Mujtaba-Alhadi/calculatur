2a90677054d3497958f0185b3766eb92f498cb15
mujtaba-alhadi <mujtabaalhadi22@gmail.com>
2025-08-08 02:50:39 +0300

    finished the main logic

--- main.js
+++ main.js
@@ -6,52 +6,37 @@ const dotBtn = document.querySelector(".dot-btn");
 const equalBtn = document.querySelector(".equal-btn");
 const digits = document.querySelectorAll(".digit");
 const operators = document.querySelectorAll(".operator");
-const operatorsArr = [...operators].map((el) => el.textContent);
-
-function add(num1, num2) {
-  return num1 + num2;
-}
-function subtract(num1, num2) {
-  return num1 - num2;
-}
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-function divide(num1, num2) {
-  return num1 / num2;
-}
-function mod(num1, num2) {
-  return num1 % num2;
-}
+const operatorsArr = ["×", "÷", "%", "+", "−"];
 
 function operate(operator, num1, num2) {
   if (operator === "+") {
-    return add(num1, num2);
-  } else if (operator === "-") {
-    return subtract(num1, num2);
-  } else if (operator === "*") {
-    return multiply(num1, num2);
-  } else if (operator === "/") {
-    return divide(num1, num2);
+    return num1 + num2;
+  } else if (operator === "−") {
+    return num1 - num2;
+  } else if (operator === "×") {
+    return num1 * num2;
+  } else if (operator === "÷") {
+    return num1 / num2;
   } else if (operator === "%") {
-    return mod(num1, num2);
+    return num1 % num2;
   }
 }
 
 function display(btn) {
-  lastInput = userInput.textContent[userInput.textContent.length - 1];
+  let input = userInput.textContent.split(" ").join("");
+  let lastInput = input[input.length - 1];
   if (
     userInput.textContent === "0" &&
-    !operatorsArr.includes(btn.textContent) &&
-    btn !== dotBtn
+    !operatorsArr.includes(btn.textContent.trim()) &&
+    btn.textContent !== "."
   ) {
     userInput.textContent = "";
     userInput.textContent += btn.textContent;
   } else if (
-    operatorsArr.includes(btn.textContent) &&
+    operatorsArr.includes(btn.textContent.trim()) &&
     operatorsArr.includes(lastInput)
   ) {
-    userInput.textContent = userInput.textContent.slice(0, -1);
+    userInput.textContent = userInput.textContent.slice(0, -3);
     userInput.textContent += btn.textContent;
   } else {
     userInput.textContent += btn.textContent;
@@ -68,13 +53,43 @@ dotBtn.addEventListener("click", (e) => display(e.target));
 
 allClear.addEventListener("click", () => {
   userInput.textContent = "0";
+  result.textContent = "";
 });
+
 backspace.addEventListener("click", () => {
+  let lastInput = userInput.textContent[userInput.textContent.length - 1];
   if (userInput.textContent.length === 1) {
     userInput.textContent = "0";
+    result.textContent = "";
   } else {
     userInput.textContent = userInput.textContent.slice(0, -1);
   }
+
+  if (lastInput === " ") {
+    userInput.textContent = userInput.textContent.slice(0, -2);
+  }
 });
 
-equalBtn.addEventListener("click", () => {});
+equalBtn.addEventListener("click", () => {
+  let equation = userInput.textContent.split(" ");
+  let num1, num2, op, opIndex, equationResult;
+
+  for (let i = 0; i < operatorsArr.length; i++) {
+    while (equation.includes(operatorsArr[i])) {
+      opIndex = equation.indexOf(operatorsArr[i]);
+      num1 = Number(equation[opIndex - 1]);
+      op = equation[opIndex];
+      num2 = Number(equation[opIndex + 1]);
+      equationResult = operate(op, num1, num2);
+      equation.splice(opIndex - 1, 3, equationResult);
+    }
+  }
+
+  if (equationResult === Infinity) {
+    result.textContent = ":)";
+  } else if (!Number.isInteger(equationResult)) {
+    result.textContent = equationResult.toFixed(2);
+  } else {
+    result.textContent = equationResult;
+  }
+});
